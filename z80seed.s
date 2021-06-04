643c6aae1bf5102c8c77e4718810b712fc33f3f3
Daniel Balko <inxonic@users.noreply.github.com>
2021-06-05 01:08:53 +0200

    Add comments to code
    also remove extra output in jump function

--- z80seed.s
+++ z80seed.s
@@ -7,9 +7,9 @@ buffer:
 	.blkb	4
 
 	.area	_HEADER (ABS)
-	;; Reset vector
 	.org 	0x00
 
+	; This is FF and can be overwritten in the ROM.
 	rst	0x38
 	rst	0x38
 	rst	0x38
@@ -19,6 +19,8 @@ buffer:
 	rst	0x38
 	rst	0x38
 
+	; Write the null terminated string at HL to the serial interface.
+	; modifies: AF, C, HL
 	.org	0x08
 puts:
 	ld	a, (hl)
@@ -29,6 +31,8 @@ puts:
 	inc	hl
 	jr	puts
 
+	; Read one character from serial interface and store it to A.
+	; modifies: AF, C
 	.org	0x10
 getchar:
 	in	a, (ace_lsr)
@@ -36,7 +40,10 @@ getchar:
 	jr	nc, getchar
 	in	a, (ace_rbr)
 	ld	c, a
+	; Fall through to putchar for remote echo.
 
+	; Write the character in C to the serial interface.
+	; modifies: AF
 	.org	0x18
 putchar:
 	in	a, (ace_lsr)
@@ -50,6 +57,10 @@ ldr_str:
 	.ascii	"\nLDR "
 	.db	0x00
 
+	; Read B hex bytes from the serial interface and store them at DE.
+	; Add each byte to L for a checksum.
+	; Clear carry flag on success, set carry flag on invalid data.
+	; modifies: AF, BC, DE, L
 	.org	0x28
 readbytes:
 	call	getxdigit
@@ -63,16 +74,22 @@ readbytes:
 	call	getxdigit
 	pop	bc
 	jr	readbytes_2
+	; Continue in code segment due to space limitation.
 
+
+	; Code location should be 0x0038.
 	.area	_CODE
 init:
-	;; Set stack pointer directly above top of memory.
+	; Set stack pointer directly above top of memory.
 	ld	sp, #0x0000
+
+	; Return to init if the called program returns.
 	ld	hl, #0x0038
 	push	hl
 
-	;; Initialize serial: 9600 Baud, 8N1
+	; Initialize the serial interface.
 
+	; 9600,8N1 @ 7.3728 MHz
 	ld	a, #1 << ace_dlab | #1 << ace_wsl1 | #1 << ace_wsl0
 	out	(ace_lcr), a
 	ld	a, #<ace_divisor
@@ -83,10 +100,11 @@ init:
 	out	(ace_lcr), a
 	ld	a, #'\n
 	out	(ace_thr), a
-
 prompt:
 	ld	hl, #ldr_str
 	rst	0x08
+
+	; Wait for input from the serial interface or timeout.
 	ld	bc, #6 << 8 | <'.
 1$:
 	in	a, (ace_lsr)
@@ -100,6 +118,7 @@ prompt:
 	djnz	1$
 	jp	0x7c00
 
+	; Continuation of subroutine at reset vector.
 readbytes_2:
 	ret	c
 	add	c
@@ -111,6 +130,7 @@ readbytes_2:
 	xor	a
 	ret
 
+	; Read Intel HEX from serial and store data to memory.
 writemem:
 	ld	de, #buffer
 	ld	l, #0
@@ -120,6 +140,8 @@ writemem:
 	jr	z, 1$
 	cp	#':
 	jr	nz, jump
+
+	; Read byte count, address and record type.
 	ld	b, #4
 	rst	0x28
 	jr	c, error
@@ -133,31 +155,40 @@ writemem:
 	inc	hl
 	ld	h,(hl)
 	ld	l, a
+
+	; Verfify mode, accept 00 and 01.
 	ld	a, h
 	srl	a
 	ld	c, #'m
 	jr	nz, ihx_error
+
+	; Skip data block if byte count is zero, inherit A as zero.
 	add	b
 	jr	z, 3$
+
+	; Read data bytes.
 	rst	0x28
 	jr	c, error
 3$:
+	; Read one checksum byte, inherit B as zero from previous loop.
 	ld	de, #buffer
 	inc	b
 	rst	0x28
 	jr	c, error
 
-	; verify checksum
+	; Verify checksum, expect zero after each line.
 	ld	a, l
 	or	a
 	ld	c, #'c
 	jr	nz, ihx_error
 
-	; expect line to end
+	; Expect line to end.
 	rst	0x10
 	ld	c, #'t
 	call	check_newline
 	jr	nz, ihx_error
+
+	; Check for end of file record type.
 	dec	h
 	jr	nz, writemem
 
@@ -173,6 +204,7 @@ error:
 	rst	0x08
 	jp	prompt
 
+	; Read hex address from serial interface and jump to it.
 jump:
 	cp	#'$
 	jr	nz, error
@@ -182,7 +214,6 @@ jump:
 	rst	0x10
 	call	check_newline
 	jr	nz, error
-	rst	0x18
 	ex	de, hl
 	dec	hl
 	ld	e, (hl)
@@ -193,12 +224,9 @@ jump:
 	ex	de, hl
 	jp	(hl)
 
-check_newline:
-	cp	#'\r
-	ret	z
-	cp	#'\n
-	ret
-
+	; Read one xdigit from serial interface and store its value to A.
+	; Clear carry flag on success, set carry flag on invalid input.
+	; modifies: AF, C
 getxdigit:
 	rst	0x10
 	sub	#0x30
@@ -206,12 +234,19 @@ getxdigit:
 	cp	#0x0a
 	ccf
 	ret	nc
+	; Convert character to upper case.
 	res	5, a
 	cp	#0x11
 	ret	c
 	add	#0xe9
 	ret	c
 	sub	#0xf0
+	ret
+
+check_newline:
+	cp	#'\r
+	ret	z
+	cp	#'\n
 	ret
 
 error_str:
